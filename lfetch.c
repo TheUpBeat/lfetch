f305f2a75db8e63c7286583d3ccca33779b9b893
TheUpBeat <TheUpBeat@protonmail.com>
2020-08-29 12:56:26 +0530

    Remove ^*^

--- lfetch.c
+++ lfetch.c
@@ -48,8 +48,6 @@ int main()
 	const double mb = 1024 * 1024;
 	double used_mb = (si.totalram / mb) - (si.freeram / mb) - (si.bufferram / mb) - (si.sharedram / mb) - (si.sharedram / mb);
 
-	printf(" ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ \n");
-
 	// Prints user@host
 	printf("       .---.	 %s@%s \n", user, hostname);
 
@@ -103,6 +101,5 @@ int main()
 
 	// Prints free RAM (include cache ram, as it cannot be removed)
 	//printf ("	RAM: \t\t%5.1f MB\n", used_mb);
-	printf(" ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ \n");
 	return 0;
 }
